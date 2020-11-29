11010044f10261d3762504fda6bc23e8ca4f81bc
leadensky <ben@leadensky.com>
2020-11-28 17:17:22 -0700

    End of Section 6

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import { Item, Button, Label, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
+  deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+  submitting: boolean;
+  target: string;
 }
 
 export const ActivityList: React.FC<IProps> = ({
   activities, 
   selectActivity,
-  deleteActivity
+  deleteActivity,
+  submitting,
+  target
 }) => {
     return (
       <Segment clearing>
@@ -35,10 +39,12 @@ export const ActivityList: React.FC<IProps> = ({
                     onClick={() => selectActivity(activity.id)}
                   />
                   <Button
+                    name={activity.id}
                     floated="right"
                     content="Delete"
                     color="red"
-                    onClick={() => deleteActivity(activity.id)}
+                    onClick={(e) => deleteActivity(e, activity.id)}
+                    loading={target === activity.id && submitting}
                   />
                   <Label basic content={activity.category} />
                 </Item.Extra>
