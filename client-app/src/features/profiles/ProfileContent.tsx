b35da9479ceb86c9fe1efbbb75fcf6365fb61770
leadensky <ben@leadensky.com>
2021-02-23 18:45:14 -0700

    Section 40, End of 526

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Tab } from "semantic-ui-react";
+import { ProfilePhotos } from "./ProfilePhotos";
 
 const panes = [
   { menuItem: "About", render: () => <Tab.Pane>About</Tab.Pane> },
-  { menuItem: "Photos", render: () => <Tab.Pane>Photos</Tab.Pane> },
+  { menuItem: "Photos", render: () => <ProfilePhotos /> },
   { menuItem: "Activities", render: () => <Tab.Pane>Activities</Tab.Pane> },
   { menuItem: "Followers", render: () => <Tab.Pane>Followers</Tab.Pane> },
   { menuItem: "Following", render: () => <Tab.Pane>Following</Tab.Pane> },
@@ -14,6 +15,7 @@ export const ProfileContent = () => {
       menu={{ fluid: true, vertical: true }}
       menuPosition="right"
       panes={panes}
+      activeIndex={1}
     />
   );
 };
